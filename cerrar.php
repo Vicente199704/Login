933f0a464fce831db7bba61ce865a931841c2583
Vicente Molina <molina.machado@hotmail.com>
2022-02-23 23:43:31 -0300

    Version 1

---
+++ cerrar.php
@@ -0,0 +1,11 @@
+<?php
+
+session_start();
+
+session_destroy();
+
+$_SESSION = array(); //Se limpia la session
+
+header('Location: login.php');
+
+?>
---
+++ views/contenido.view.php
@@ -0,0 +1,41 @@
+<?php
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>contenido</title>
+    <link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css">
+    <link rel="stylesheet" href="css/estilos.css">
+
+</head>
+
+<body>
+    <div class="contenedor">
+        <h1 class="titulo"> Contenido del Sitio</h1>
+        <a href="cerrar.php">Cerrar Sesion</a>
+        <hr class="border">
+        <div class="contenido">
+            <article>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus minima obcaecati, earum similique,
+                    quos saepe molestias ducimus provident fuga doloribus dicta impedit suscipit nobis! Incidunt eveniet
+                    quis exercitationem quas vel?
+                </p>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Error eum molestias nam quia? Tempore
+                    necessitatibus sapiente deleniti nemo vel quam ex, non vitae reiciendis quidem tempora praesentium
+                    corporis error delectus.
+                </p>
+            </article>
+        </div>
+    </div>
+
+</body>
+
+</html>
